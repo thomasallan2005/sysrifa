75cc12e7fd9f7aa3c0a11ec41b76eba7ac7cff39
thomasallan2005 <allan.bunny05@gmail.com>
2022-05-30 13:51:30 -0300

    Update conn.php

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -1,7 +1,7 @@
 <?php
 
     // Carregar as credenciais do banco de dados
-    $hostname = "";
-    $database = "";
-    $user = "";
-    $password = "";
+    $hostname = "sql212.epizy.com";
+    $database = "epiz_31820076_sysrifa";
+    $user = "epiz_31820076";
+    $password = "cVGDozGny8ux";
