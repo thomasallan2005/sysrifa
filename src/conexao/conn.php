09617330a42d66fcfe3dbb788b201ae7e8feb980
thomasallan2005 <106342905+thomasallan2005@users.noreply.github.com>
2022-05-30 20:46:31 -0300

    Commit 300522

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -1,7 +1,15 @@
 <?php
 
     // Carregar as credenciais do banco de dados
-    $hostname = "";
-    $database = "";
-    $user = "";
-    $password = "";
+    $hostname = "sql212.epizy.com";
+    $database = "epiz_31820076_sysrifa";
+    $user = "	epiz_31820076";
+    $password = "cVGDozGny8ux";
+
+    try {
+        $pdo = new PDO('mysql:host='.$hostname.';dbname='.$database, $user, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo 'Conexão com o banco de dados '.$database.' foi realizado com sucesso!';
+    } catch (PDOException $e) {
+        echo 'Erro: '.$e->getMessage();
+    }
